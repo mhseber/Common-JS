9222117ba608b23a8b17efa540f8ee9b27ba24b1
mahamudul hasan seber <sebermh@gmail.com>
2026-02-08 19:09:57 +0600

    Null and Undefined

---
+++ undefined.js
@@ -0,0 +1,16 @@
+// Null and Undefined
+
+let money;
+// console.log(money); // Output: undefined
+
+function total(a,d,c,){
+    // console.log(a,d,c);
+}
+total(2)
+
+
+const group = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F'];
+// console.log(group[19]);
+delete group[2];
+console.log(group[6]); 
+
