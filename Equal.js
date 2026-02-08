426c480552f02577c1aafea7161aad58ca947dfb
mahamudul hasan seber <sebermh@gmail.com>
2026-02-08 19:46:06 +0600

    Double Equal (==) and Triple Equal (===) in JavaScript

---
+++ Equal.js
@@ -0,0 +1,29 @@
+// Double Equal (==) and Triple Equal (===) in JavaScript
+
+
+//  ===
+console.log(5 === "5");
+console.log(5 === 5);
+console.log(5 === 4);
+console.log(5 === 'seber');
+console.log('seber' === 'seber');
+console.log(true === true);
+console.log(5 === true);
+console.log(5 === "5");
+console.log(5 === 22);
+console.log(22 === 22);
+
+console.log('seber---------------------');
+
+// ==
+
+console.log(5 == "5");
+console.log(5 == 5);
+console.log(5 == 4);
+console.log(5 == 'seber');
+console.log('seber' == 'seber');
+console.log(true == true);
+console.log(5 == true);
+console.log(5 == "5");
+console.log(5 == 22);
+console.log(22 == 22);
