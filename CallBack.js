8cce3799f2933d5920d17a7852875278ab7b2745
mahamudul hasan seber <sebermh@gmail.com>
2026-02-08 21:16:16 +0600

    CallBack

---
+++ CallBack.js
@@ -0,0 +1,12 @@
+// CallBack Function
+
+function myFunc(name, callBack){
+    if(patri){
+        console.log('Hello', name);
+    }
+}
+
+function callBack(person){
+    console.log('I am a call back function', person);
+}
+callBack('seber');
